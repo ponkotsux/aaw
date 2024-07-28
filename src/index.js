d2e1fc3931d61c079e138281e402893b32961797
ponkotsux <ponkotsux7@gmail.com>
2024-07-28 17:51:26 +0900

    Rebuild and deploy Gacha Simulator

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GachaSimulator from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GachaSimulator />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
